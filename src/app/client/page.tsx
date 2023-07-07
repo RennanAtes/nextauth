7242b477fab75e6e5a6b8ef02d530b06f9b5d627
RennanAtes <RennanAugustoBR@Gmail.com>
2023-07-07 10:31:37 -0300

    Enviando a requisicao com o token do Django

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -15,27 +15,30 @@ export default function Client(){
     });
     console.log(session)
 
-    // async function teste_api(){
-    //     const headers = {
-    //         Authorization: `Bearer ${session.accessToken}`,
-    //     };
-    //     try {
+    async function teste_api(){
         
-    //     const response = await axios.post('http://127.0.0.1:8000/api/teste', { headers });
-    //     console.log(response.data)
-    //     } catch (error) {
-    //         console.log(error)
-    //     }
+        try {
+
+            const headers = {
+                Authorization: `Bearer ${session.Django}`,
+              };
+
+            const response = await axios.post('http://127.0.0.1:8000/api/teste', {}, { headers });
+            console.log(response.data)
+            
+        } catch (error) {
+            console.log(error)
+        }
         
-    // }
+    }
     return(
         <div>
             { session ? 
             <div>
-                <p>{ session.accessToken }</p>
-                {/* <button type="button" onClick={teste_api}>
+                <p>{ session.Django }</p>
+                <button type="button" onClick={teste_api}>
                     Teste
-                </button>  */}
+                </button> 
             </div>
             : 
             
